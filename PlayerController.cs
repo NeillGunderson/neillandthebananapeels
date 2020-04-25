c05c3b02b20fd067b5711336ee891965c9d00fea
NeillGunderson <60712328+NeillGunderson@users.noreply.github.com>
2020-04-24 20:59:57 -0500

    This is Version 2.0
    -added more movement features 
    -new animations
    -collectible items

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,25 +4,91 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-   private void Update()
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State {idle, running, jumping, falling}
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+
+    public int cherries = 0;
+    private void Start()
     {
-        if(Input.GetKey(KeyCode.RightArrow))
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+    private void Update()
+    {
+
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if (hDirection < 0)
         {
-            rb.velocity = new Vector2(5, 0);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
 
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (hDirection > 0)
         {
-            rb.velocity = new Vector2(-5, 0);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
 
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            rb.velocity = new Vector2(0, 5);
 
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+
+        AnimationState();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void AnimationState()
+    {
+        if (state == State.jumping)
+        {
+            if (rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }       
+        }
+
+        else if (state == State.falling)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //Moving to the right
+            state = State.running;
+        }
+
+        else
+        {
+            state = State.idle;
+        }
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collectable")
+        {
+            Destroy(collision.gameObject);
+            cherries += 1;
         }
     }
+
 }
+
 
